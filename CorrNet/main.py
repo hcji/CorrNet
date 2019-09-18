5972da24eddbc7157cff8408b0729d8180119262
hcji <ji.hongchao@foxmail.com>
2019-09-18 16:14:05 +0800

    add test

--- CorrNet/main.py
+++ CorrNet/main.py
@@ -34,24 +34,42 @@ class ZeroPadding(Layer):
         return input_shape
     
     
-def correlationLoss(fake, H):
-    hdim_deep = int(H.shape[1].value/2)
-    y1 = H[:,:hdim_deep]
-    y2 = H[:,hdim_deep:]
-    y1_mean = K.mean(y1, axis=0)
-    y1_centered = y1 - y1_mean
-    y2_mean = K.mean(y2, axis=0)
-    y2_centered = y2 - y2_mean
-    corr_nr = K.sum(y1_centered * y2_centered, axis=0) 
-    corr_dr1 = K.sqrt(K.sum(y1_centered * y1_centered, axis=0) + 1e-8)
-    corr_dr2 = K.sqrt(K.sum(y2_centered * y2_centered, axis=0) + 1e-8)
-    corr_dr = corr_dr1 * corr_dr2
-    corr = corr_nr / corr_dr 
-    return K.sum(corr) * -0.02
+class CorrnetCost(Layer):
+    def __init__(self,lamda, **kwargs):
+        super(CorrnetCost, self).__init__(**kwargs)
+        self.lamda = lamda
+
+    def cor(self,y1, y2, lamda):
+        y1_mean = K.mean(y1, axis=0)
+        y1_centered = y1 - y1_mean
+        y2_mean = K.mean(y2, axis=0)
+        y2_centered = y2 - y2_mean
+        corr_nr = K.sum(y1_centered * y2_centered, axis=0)
+        corr_dr1 = K.sqrt(K.sum(y1_centered * y1_centered, axis=0) + 1e-8)
+        corr_dr2 = K.sqrt(K.sum(y2_centered * y2_centered, axis=0) + 1e-8)
+        corr_dr = corr_dr1 * corr_dr2
+        corr = corr_nr / corr_dr
+        return K.sum(corr) * lamda
+
+    def call(self ,x ,mask=None):
+        h1=x[0]
+        h2=x[1]
+        corr = self.cor(h1,h2,self.lamda)
+        #self.add_loss(corr,x)
+        #we output junk but be sure to use it for the loss to be added
+        return corr
+    def get_output_shape_for(self, input_shape):
+        #print input_shape[0][0]
+        return (input_shape[0][0],input_shape[0][1])
+
+def corr_loss(y_true, y_pred):
+    #print y_true.type,y_pred.type
+    #return K.zeros_like(y_pred)
+    return y_pred
 
 
 class CorrNet:
-    def __init__(self, data_l, data_r, nb_epoch=10):
+    def __init__(self, data_l, data_r, Lambda=0.02, nb_epoch=10):
         self.data_l = data_l
         self.data_r = data_r
         self.nb_epoch = nb_epoch
@@ -83,11 +101,11 @@ class CorrNet:
         [recx1,recy1,h1] = branchModel([inpx, ZeroPadding()(inpy)])
         [recx2,recy2,h2] = branchModel([ZeroPadding()(inpx), inpy])
         [recx3,recy3,h] = branchModel([inpx, inpy])
-        H= concatenate([h1,h2])
+        corr = CorrnetCost(-Lambda)([h1,h2])
         
         opt = optimizers.Adam(lr=0.01)
-        model = Model([inpx,inpy],[recx1,recx2,recx3,recy1,recy2,recy3,H])
-        model.compile(loss=["mse","mse","mse","mse","mse","mse",correlationLoss], optimizer=opt)
+        model = Model([inpx,inpy],[recx1,recx2,recx3,recy1,recy2,recy3,corr])
+        model.compile(loss=["mse","mse","mse","mse","mse","mse",corr_loss], optimizer=opt)
         self.model = model
         self.branchModel = branchModel
         
@@ -108,12 +126,18 @@ class CorrNet:
         new_data_l,_,_ = branchModel.predict([np.zeros(new_data_r.shape), new_data_r])
         return new_data_l
     
-    def left_right_corr(self, new_data_l, new_data_r):
+    def left_to_latent(self, new_data_l):
         branchModel = self.branchModel
-        _,_,new_H = branchModel.predict([new_data_l, new_data_r])
-        dimh = int(new_H.shape[1]/2)
-        v1 = new_H[:,:dimh]
-        v2 = new_H[:,dimh:]
-        corrs = [pearsonr(v1[i], v2[i])[0] for i in range(len(v1))]
-        return corrs
+        _,_,h = branchModel.predict([new_data_l, np.zeros(new_data_l.shape)])
+        return h
+        
+    def right_to_latent(self, new_data_r):
+        branchModel = self.branchModel
+        _,_,h = branchModel.predict([np.zeros(new_data_r.shape), new_data_r])
+        return h
+
+    def both_to_latent(self, new_data_l, new_data_r):
+        branchModel = self.branchModel
+        _,_,h = branchModel.predict([new_data_l, new_data_r])
+        return h        
     
---
+++ CorrNet/test.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 18 10:41:30 2019
+
+@author: hcji
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+from CorrNet.main import CorrNet
+
+data_l = np.load('Data/data_l.npy')
+data_r = np.load('Data/data_r.npy')
+label = np.load('Data/data_label.npy')
+
+test_l = np.load('Data/test_v1.npy')
+test_r = np.load('Data/test_v2.npy')
+test_label = np.load('Data/test_l.npy')
+
+corrnet = CorrNet(data_l, data_r)
+corrnet.train()
+rebu_l = corrnet.right_to_left(test_r)
+rebu_r = corrnet.left_to_right(test_l)
+
+def visualize(left, right):
+    img_lef = left.reshape((28,14))
+    img_rig = right.reshape((28,14))
+    f, axarr = plt.subplots(1,2,sharey=False)
+    axarr[0].imshow(img_lef)
+    axarr[1].imshow(img_rig)
+
+# from left to right
+left = test_l[0]
+right = rebu_r[0]
+visualize(left, right)
+
+# from right to left
+left = rebu_l[0]
+right = test_r[0]
+visualize(left, right)
+
+# latent correlation
+latent1 = corrnet.left_to_latent(test_r)
+latent2 = corrnet.left_to_latent(test_l)
+pearsonr(latent1[4], latent2[4])
